23293052f7b97d24f82786b6077f8764386bbc6c
qiushaowei <qiushaowei@360.net>
2018-12-07 15:54:13 +0800

    init project

---
+++ bin/data/constant.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var APPNAME = exports.APPNAME = 'mergefolder';
+var TITLE = exports.TITLE = 'merge sub folder into new folder';
+
+var PROJECT_ROOT_NOT_EXISTS = exports.PROJECT_ROOT_NOT_EXISTS = "找不到项目根目录, 请指定有效的目录";
+
+var PROJECT_UNRECOGNIZED = exports.PROJECT_UNRECOGNIZED = "无法识别的系统";
